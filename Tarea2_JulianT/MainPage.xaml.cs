c3737e2ecf3d2f97ca0b2f7fcfa87330b02b481d
Julian Tapia <Julian Tapia@JULY10435>
2021-10-29 19:23:42 -0500

    Agregar archivos de proyecto.

---
+++ Tarea2_JulianT/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea2_JulianT
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(string Usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = Usuario;
+        }
+
+        private void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double SeguimientoUno = Convert.ToDouble(txtS1.Text);
+                double ExamenUno = Convert.ToDouble(txtE1.Text);
+
+                double N1 = SeguimientoUno * 0.3;
+                double E1 = ExamenUno * 0.2;
+
+                double suma = N1 + E1;
+                txtPa1.Text = Convert.ToString(suma);
+
+                double SeguimientoDos = Convert.ToDouble(txtS2.Text);
+                double ExamenDos = Convert.ToDouble(txtE2.Text);
+
+                double N2 = SeguimientoDos * 0.3;
+                double E2 = ExamenDos * 0.2;
+
+                double suma2 = N2 + E2;
+                txtPa2.Text = Convert.ToString(suma2);
+
+                double NotaFinal = suma + suma2;
+                txtNf.Text = Convert.ToString(NotaFinal);
+
+                if (NotaFinal >= 7)
+                {
+                    txtEstado.Text = Convert.ToString("Aprobado");
+                }
+                else
+                {
+                    txtEstado.Text = Convert.ToString("Reprobado");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Advertencia", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ Tarea2_JulianT/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea2_JulianT
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void vtnIngresar_Clicked(object sender, EventArgs e)
+        {
+            String Usuario = txtUsuario.Text;
+            string Contraseña = txtContraseña.Text;
+            if(Usuario=="Estudiante2021" && Contraseña == "UIsrael2021")
+            {
+                await Navigation.PushAsync(new MainPage(Usuario));
+            }
+        }
+    }
+}
